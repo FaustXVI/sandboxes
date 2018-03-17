f1b7a3887dadcc757f442058a48cc6256c451b4c
FaustXVI <xavier.detant@gmail.com>
2018-03-17 14:08:56 +0100

    tests are running

--- elm/default.nix
+++ elm/default.nix
@@ -1,7 +1,24 @@
 with import <nixpkgs> {};
-stdenv.mkDerivation {
+let
+  my-glibc = glibc;
+  my-gcc = gcc-unwrapped.lib;
+  my-gmp = gmp;
+in stdenv.mkDerivation {
     name = "elm-sandbox";
     buildInputs = [
         elmPackages.elm
-    ];
+            nodejs
+   ];
+    patchPhase = ''
+        patchShebangs node_modules/webpack
+        '';
+    shellHook = ''
+        export PATH="$PATH:$(pwd)/node_modules/.bin"
+        patchelf \
+            --debug \
+            --set-interpreter ${my-glibc}/lib/ld-linux-x86-64.so.2 \
+            --set-rpath "${builtins.concatStringsSep ":" (map (l: "${l}/lib") [my-gcc my-gmp my-glibc])}" \
+            node_modules/elm-test/bin/elm-interface-to-json
+        '';
+
 }
