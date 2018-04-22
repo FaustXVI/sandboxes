c7fe5f897e2d4722cbb1a2da9ea60fdc1143a073
FaustXVI <xavier.detant@gmail.com>
2018-04-22 12:03:35 +0200

    Use rustup for rust

--- rust/default.nix
+++ rust/default.nix
@@ -3,8 +3,7 @@ with import <nixpkgs> {};
     rustEnv = stdenv.mkDerivation {
         name = "rust-sandbox";
         buildInputs = [
-            rustc
-            cargo
+            rustup
         ];
     };
 }
