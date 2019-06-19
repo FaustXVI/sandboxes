189e61ba96fe4c28fb5c3d56ef009155f6fe7e02
FaustXVI <xavier.detant@gmail.com>
2019-06-19 11:10:03 +0200

    update jdk for kotlin

--- kotlin/default.nix
+++ kotlin/default.nix
@@ -3,9 +3,10 @@ with import <nixpkgs> {};
     javaEnv = stdenv.mkDerivation {
         name = "kotlin-sandbox";
         buildInputs = [
-                pkgs.openjdk9
+                pkgs.openjdk11
                 pkgs.maven
                 pkgs.gradle
+                pkgs.kotlin
         ];
     };
 }
