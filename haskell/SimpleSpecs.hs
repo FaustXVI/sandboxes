beaf7b07cce28bfe35df602310df31607358aafe
FaustXVI <xavier.detant@gmail.com>
2019-08-14 10:40:29 +0200

    Add quickcheck exemple

--- haskell/SimpleSpecs.hs
+++ haskell/SimpleSpecs.hs
@@ -1,6 +1,11 @@
 import Test.Hspec;
+import Test.QuickCheck;
 
 main = hspec $ do
     describe "a simple test" $ do
         it "should work" $ do
             2 + 2 `shouldBe` 4
+        it "should be associative" $ do
+            let f = (\x y -> x + y == y + x) :: Int -> Int -> Bool
+            property f
+
