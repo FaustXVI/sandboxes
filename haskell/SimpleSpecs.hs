ffd18cba2d0d9d6b8dcb40b35c1a44eb97712e1c
FaustXVI <xavier.detant@gmail.com>
2019-08-24 15:07:19 +0200

    Getting Ready

--- haskell/SimpleSpecs.hs
+++ haskell/SimpleSpecs.hs
@@ -8,4 +8,9 @@ main = hspec $ do
         it "should be associative" $ do
             let f = (\x y -> x + y == y + x) :: Int -> Int -> Bool
             property f
-
+        it "should validate property" $ do
+            let f = (\n -> even n ==> odd (n+1)) :: Int -> Property
+            property f
+        it "should validate property" $ do
+            let f = (\n -> n > 0 && n < 12 ==> n + 1 > 0 && n <= 12) :: Int -> Property
+            property f
--- java/src/main/java/SimpleCode.java
+++
@@ -1,5 +0,0 @@
-public class SimpleCode {
-    static int get42() {
-        return 42;
-    }
-}
